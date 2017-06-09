bcda80e2349386da03aa45e5c73406dae45d441e
Femi Oni <freefony@gmail.com>
2017-06-09 11:46:41 +0100

    list of books and their authors

---
+++ javascript/books.js
@@ -0,0 +1,64 @@
+var books = [
+  {
+    title: 'Nineteen Eighty-Four',
+    author: 'George Orwell',
+    category: 'politics',
+    ratings: 5.0
+  },
+  {
+    title: 'Animal Farm',
+    author: 'George Orwell',
+    category: 'politics',
+    ratings: 5.0
+  },
+  {
+    title: 'After Doomsday',
+    author: 'Poul Anderson',
+    category: 'scifi',
+    ratings: 3.5
+  },
+  {
+    title: 'Against the Fall of Night',
+    author: 'Arthur C. Clarke',
+    category: 'scifi',
+    ratings: 3.0
+  },
+  {
+    title: 'Ancient Shores',
+    author: 'Jack McDevitt',
+    category: 'scifi',
+    ratings: 4.0
+  },
+  {
+    title: 'When it\'s real',
+    author: 'Erin Watt',
+    category: 'romance',
+    ratings: 4.0
+  },
+  {
+    title: 'Sized by seduction',
+    author: 'Brenda Jackson',
+    category: 'romance',
+    ratings: 4.5
+  },
+  {
+    title: 'A lover\'s vow',
+    author: 'Brenda Jackson',
+    category: 'romance',
+    ratings: 5.0
+  },
+  {
+    title: 'Exit Strategy',
+    author: 'Steve Hamilton',
+    category: 'crime',
+    ratings: 5.0
+  },
+  {
+    title: 'G-man',
+    author: 'Stephen Hunter',
+    category: 'crime',
+    ratings: 3.8
+  },
+]
+
+export default book
